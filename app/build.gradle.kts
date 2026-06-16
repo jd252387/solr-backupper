328ebd7fc85beac8eb5730bfa15fac6f27e28fe2
John Doe <jd252387@gmail.com>
2026-06-16 07:57:30 +0300

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,6 +8,8 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
+    id("org.springframework.boot") version "3.2.5"
+    id("io.spring.dependency-management") version "1.1.4"
 }
 
 repositories {
@@ -21,6 +23,14 @@ dependencies {
 
     // This dependency is used by the application.
     implementation(libs.guava)
+    compileOnly("org.projectlombok:lombok:1.18.46")
+	annotationProcessor("org.projectlombok:lombok:1.18.46")
+
+	testCompileOnly("org.projectlombok:lombok:1.18.46")
+	testAnnotationProcessor("org.projectlombok:lombok:1.18.46")
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("io.projectreactor:reactor-core")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
